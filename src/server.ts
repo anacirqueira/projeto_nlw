a73de3d0ae64648dca37502b31c74e8dde14aca6
anacirqueira <cirqueiraana6@gmail.com>
2021-04-22 17:32:22 -0300

    Atuallizando...

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,7 @@
 import express from "express";
-
+import "./database";
+import { routes } from "./routes";
 const app = express();
-/**
- * GET = buscas 
- * POST = criação 
- * PUT = alteração
- * DELETE = delete
- * PATCH = alterar uma info específica
- */
-app.get("/", (request, response) => {
-  return response.json({
-    message: "Olá NLW 05",
-  });
-});
-app.post("/", (request, response) => {
-  return response.json({ message: "Usuário salvo!" });
-});
-
+app.use(express.json());
+app.use(routes);
 app.listen(3333, () => console.log("Server is running on port 3333"));
