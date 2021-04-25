89a944b4c08899d76236783411bf66aa3e47e90f
anacirqueira <cirqueiraana6@gmail.com>
2021-04-24 21:23:37 -0300

    Dia 4

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,27 @@
 import express from "express";
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
 import "./database";
 import { routes } from "./routes";
+import path from "path";
+
 const app = express();
+
+app.use(express.static(path.join(__dirname, "..", "public")));
+app.set("views", path.join(__dirname, "..", "public"));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+
+app.get("/pages/client", (request, response) => {
+  return response.render("html/client.html");
+});
+
+const http = createServer(app); // Criando o protocolo http
+const io = new Server(http); // Criando o protocolo WS
+
+io.on("connection", (socket: Socket) => {
+  console.log("Se conectou", socket.id);
+});
 app.use(express.json());
 app.use(routes);
-app.listen(3333, () => console.log("Server is running on port 3333"));
+http.listen(3333, () => console.log("Server is running on port 3333"));
